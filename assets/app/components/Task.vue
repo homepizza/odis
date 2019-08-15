2e54cc778cea3935975c6f1a5ccef37e60fa5151
Alex Malozemov <is.malozemov@ya.ru>
2019-08-15 12:56:55 +0500

    Unique files for upload with copy filename

--- assets/app/components/Task.vue
+++ assets/app/components/Task.vue
@@ -1,12 +1,23 @@
 <template>
-    <article class="c-stage" style="min-height: 355px;">
+    <article class="c-stage">
         <div class="c-stage__header o-media u-justify-start">
             <div class="c-stage__icon o-media__img">
                 <i class="fa fa-laptop"></i>
             </div>
-            <div class="c-stage__header-title o-media__body">
-                <h6 class="u-mb-zero">{{ task.title }}</h6>
-                <p class="u-text-xsmall u-text-mute">Была в работе: 05.08.2019 | Ожидается через: 10 дней(-я)</p>
+            <div class="c-stage__header-title o-media__body" style="width: 80%;">
+                <h6 class="u-mb-zero" v-if="!$store.state.Task.edit">{{ title }}</h6>
+                <input type="text" class="c-input large"
+                    v-if="$store.state.Task.edit"
+                    @change="setTitle"
+                    v-model="title"
+                    placeholder="Заголовк задачи"
+                    style="width: 100%;"
+                >
+                <p class="u-text-xsmall u-text-mute" v-show="false">
+                    Была в работе: 05.08.2019 | Ожидается через: 10 дней(-я)
+                    #1 Была в работе: N дней
+                    #2 Попала в работу: 05.08.2019 | Текущий статус: 06.08.2019
+                </p>
             </div>
         </div>
 
@@ -15,8 +26,16 @@
             <p class="u-text-mute u-text-uppercase u-text-small u-mb-xsmall">Описание</p>
             <div class="u-mb-medium u-text-mute u-text-small"
                  style="list-style-type: revert !important;"
-                 v-html="task.body"
+                 v-if="!$store.state.Task.edit"
+                 v-html="description"
             >
+            </div>
+            <div v-if="$store.state.Task.edit">
+                <trumbowyg v-model="description" :config="config" @tbw-blur="setDescription"
+                           class="form-control"
+                           name="description"
+                >
+                </trumbowyg>
             </div>
 
             <p class="u-text-mute u-text-uppercase u-text-small u-mb-xsmall" v-if="attachments.length !== 0">
@@ -38,13 +57,18 @@
 </template>
 
 <script>
+    import Trumbowyg from 'vue-trumbowyg';
+    import 'trumbowyg/dist/ui/trumbowyg.css';
     export default {
         name: "Task",
         data: function () {
             return {
+                config: {},
                 taskNumber: '',
                 task: '',
-                attachments: []
+                attachments: [],
+                title: '',
+                description: ''
             }
         },
         beforeCreate() {
@@ -53,8 +77,21 @@
                 if (response.status === 200) {
                     this.task = response.data;
                     this.attachments = response.data.attachments;
+                    this.title = response.data.title;
+                    this.description = response.data.body;
                 }
             });
+        },
+        methods: {
+            setTitle: function () {
+                this.$store.commit('setTitle', this.title);
+            },
+            setDescription: function () {
+                this.$store.commit('setDescription', this.description);
+            }
+        },
+        components: {
+            Trumbowyg
         }
     }
 </script>
--- src/Utils/FileUploader.php
+++ src/Utils/FileUploader.php
@@ -6,18 +6,21 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface AS TokenGen;
 
 class FileUploader
 {
     private $logger;
     private $fl;
     private $rstack;
+    private $token;
 
-    public function __construct(LoggerInterface $logger, Filesystem $fl, RequestStack $rstack)
+    public function __construct(LoggerInterface $logger, Filesystem $fl, RequestStack $rstack, TokenGen $token)
     {
         $this->logger = $logger;
         $this->fl = $fl;
         $this->rstack = $rstack;
+        $this->token = $token;
     }
 
     /**
@@ -33,12 +36,21 @@ class FileUploader
         try {
             $exist = $this->fl->exists('../'.$dir);
             if (!$exist) { $this->fl->mkdir('../'.$dir); }
-            $file->move('../'.$dir, $filename);
+            $existFile = $this->fl->exists('../'.$dir.'/'.$filename);
+            if ($existFile) {
+                $token = $this->token->generateToken();
+                $fileDir = '../'.$dir.'/'.$token;
+                $this->fl->mkdir($fileDir);
+            }
+            $dir = $existFile ? $fileDir : '../'.$dir;
+            $file->move($dir, $filename);
         } catch (FileException $e) {
             $this->logger->error('Ошибка загрузки файла' . $e->getMessage());
             throw new FileException('Ошибка загрузки файла');
         }
-        return $this->rstack->getCurrentRequest()->getUriForPath('/uploads/'.$filename);
+
+        $path = $existFile ? '/uploads/'.$token.'/'.$filename : '/uploads/'.$filename;
+        return $this->rstack->getCurrentRequest()->getUriForPath($path);
     }
 
     /**
